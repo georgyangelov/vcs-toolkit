8769f12c435ced13c6d386fe0051a1106fa762fc
Georgy Angelov <georgyangelov@gmail.com>
2013-12-20 18:31:48 +0200

    Fix the raise of NotImplementedError in ObjectManager

--- lib/vcs_toolkit/object_manager.rb
+++ lib/vcs_toolkit/object_manager.rb
@@ -4,19 +4,19 @@ module VCSToolkit
 
   class ObjectManager
     def initialize
-      raise NotImplementedError('The ObjectManager class should be inherited and its methods overriden')
+      raise NotImplementedError, 'The ObjectManager class should be inherited and its methods overriden'
     end
 
     def store(object_id)
-      raise NotImplementedError('You must implement ObjectManager#store')
+      raise NotImplementedError, 'You must implement ObjectManager#store'
     end
 
     def retrieve(object_id)
-      raise NotImplementedError('You must implement ObjectManager#retrieve')
+      raise NotImplementedError, 'You must implement ObjectManager#retrieve'
     end
 
     def include?(object_id)
-      raise NotImplementedError('You must implement ObjectManager#include?')
+      raise NotImplementedError, 'You must implement ObjectManager#include?'
     end
   end
 
