70cd4515875e66d11587ef78f30ba066ad0053ce
Georgy Angelov <georgyangelov@gmail.com>
2013-12-23 13:55:23 +0200

    Add requirement for ObjectManager to be an Enumerable
    Also expand the description of ObjectManager and its methods.

--- lib/vcs_toolkit/object_manager.rb
+++ lib/vcs_toolkit/object_manager.rb
@@ -2,20 +2,50 @@ require 'vcs_toolkit/exceptions'
 
 module VCSToolkit
   ##
-  # The methods are compatible with the interface of Hash
-  # so a hash can be used instead of a full-featured object manager.
+  # This class is used to implement a custom storage provider for
+  # objects.
+  #
+  # The methods are compatible with the interface of Hash so that
+  # a simple Hash can be used instead of a full-featured object manager.
+  #
   class ObjectManager
+    include Enumerable
+
+    ##
+    # Implement this to store a specific object in persistent storage.
+    #
     # object_id is here for compatibility with Hash
+    #
     def store(object_id, object)
       raise NotImplementedError, 'You must implement ObjectManager#store'
     end
 
+    ##
+    # Implement this to retrieve an object with the specified object_id.
+    # It should detect the object type and instantiate the specific
+    # object class (or a subclass of it).
+    #
     def fetch(object_id)
       raise NotImplementedError, 'You must implement ObjectManager#fetch'
     end
 
+    ##
+    # Implement this to detect wether a object with that object_id exists.
+    #
     def key?(object_id)
       raise NotImplementedError, 'You must implement ObjectManager#key?'
+    end
+
+    ##
+    # Implement this to enumerate over all objects that
+    # have Object#enumerable? set to true.
+    # Even enumeration of all objects is possible, but is not
+    # neccessary.
+    #
+    # The order of enumeration doesn't matter.
+    #
+    def each
+      raise NotImplementedError, 'You must implement ObjectManager#each'
     end
   end
 end
--- spec/vcs_toolkit/object_manager_spec.rb
+++ spec/vcs_toolkit/object_manager_spec.rb
@@ -7,11 +7,15 @@ describe VCSToolkit::ObjectManager do
   it { should respond_to :key?  }
   it { should respond_to :fetch }
   it { should respond_to :store }
+  it { should respond_to :each  }
+
+  it { should be_a_kind_of Enumerable }
 
   it 'raises an error on non-overriden methods' do
     expect { subject.key?(:id)        }.to raise_error(NotImplementedError)
     expect { subject.fetch(:id)       }.to raise_error(NotImplementedError)
     expect { subject.store(:id, :obj) }.to raise_error(NotImplementedError)
+    expect { subject.each { }         }.to raise_error(NotImplementedError)
   end
 
 end
