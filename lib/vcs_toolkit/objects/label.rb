d8827f25085022f4cb4c07e6c67c4162744743b4
Georgy Angelov <georgyangelov@gmail.com>
2013-12-23 23:27:37 +0200

    Add Objects::Label - the first named object
    Its idea is to be a reference to other (unnamed) objects.
    Later branches and tags will inherit from Label.

---
+++ lib/vcs_toolkit/objects/label.rb
@@ -0,0 +1,24 @@
+module VCSToolkit
+  module Objects
+
+    class Label < Object
+      attr_reader :reference_id
+
+      def initialize(name, reference_id)
+        @reference_id = reference_id
+        super name, named: true
+      end
+
+      def ==(other)
+        object_id == other.object_id and reference_id == other.reference_id
+      end
+
+      alias_method :eql?, :==
+
+      def hash
+        [object_id, reference_id].hash
+      end
+    end
+
+  end
+end
---
+++ spec/vcs_toolkit/objects/label_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe VCSToolkit::Objects::Label do
+
+  let(:label_name)   { 'HEAD' }
+  let(:reference_id) { '7044ef26b9f7e16ad4d6c9160ea427dc28997d76' }
+
+  let(:label) { described_class.new label_name, reference_id }
+
+  context 'interface' do
+    it 'has reference_id getter' do
+      expect(label.reference_id).to eq reference_id
+    end
+
+    it 'is named' do
+      expect(label.named?).to eq true
+    end
+  end
+
+  it 'should equal other labels with the same data' do
+    label_two = described_class.new label_name, reference_id
+
+    expect(label).to eq  label_two
+    expect(label).to eql label_two
+
+    expect(label.hash).to eq label_two.hash
+  end
+
+  it 'should not equal labels with different data' do
+    label_two   = described_class.new label_name, 'other_ref_id'
+    label_three = described_class.new 'master',   reference_id
+
+    expect(label).to_not eq label_two
+    expect(label).to_not eq label_three
+  end
+
+end
