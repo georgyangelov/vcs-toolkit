0c452dc008c9ef17ebd158c23a666020f66cb0e5
Georgy Angelov <georgyangelov@gmail.com>
2014-02-02 20:38:39 +0200

    Add Utils::Status.compare_trees to compare two trees

--- lib/vcs_toolkit/utils/status.rb
+++ lib/vcs_toolkit/utils/status.rb
@@ -22,6 +22,37 @@ module VCSToolkit
           {created: created_files, changed: changed_files, deleted: deleted_files}
         end
 
+        def compare_trees(base_tree, new_tree, object_store, ignore: [])
+          created_files = []
+          changed_files = []
+          deleted_files = []
+
+          if base_tree.nil? and new_tree.nil?
+            # Do nothing... No changed or deleted files.
+          elsif new_tree.nil?
+            deleted_files = base_tree.all_files(object_store, ignore: ignore).map(&:first)
+          elsif base_tree.nil?
+            created_files = new_tree.all_files(object_store, ignore: ignore).map(&:first)
+          else
+            base_files = Hash[base_tree.all_files(object_store, ignore: ignore).to_a]
+            new_files  = Hash[new_tree.all_files(object_store, ignore: ignore).to_a]
+
+            base_file_paths = base_files.keys
+            new_file_paths  = new_files.keys
+
+            created_files = new_file_paths  - base_file_paths
+            deleted_files = base_file_paths - new_file_paths
+
+            changed_files = (base_file_paths & new_file_paths).select do |file|
+              # A file has changed if the ID of it's blob is different
+              base_files[file] != new_files[file]
+            end
+
+          end
+
+          {created: created_files, changed: changed_files, deleted: deleted_files}
+        end
+
       end
     end
 
--- spec/vcs_toolkit/utils/status_spec.rb
+++ spec/vcs_toolkit/utils/status_spec.rb
@@ -12,6 +12,15 @@ describe VCSToolkit::Utils::Status do
     }
   end
 
+  let(:new_tree_files) do
+    {
+      'README_new.md'                         => '1234',
+      'lib/vcs_toolkit.rb'                    => '9678',
+      'lib/vcs_toolkit/utils/object_store.rb' => '4567',
+      'lib/vcs_toolkit/objects/object1.rb'    => '5678',
+    }
+  end
+
   let(:file_store) do
     VCSToolkit::Utils::MemoryFileStore.new({
       'README_new.md'                         => 'content 1',
@@ -28,12 +37,19 @@ describe VCSToolkit::Utils::Status do
       '3456' => VCSToolkit::Objects::Blob.new(content: 'content 3'),
       '4567' => VCSToolkit::Objects::Blob.new(content: 'content 4'),
       '5678' => VCSToolkit::Objects::Blob.new(content: 'content 5'),
+      '9678' => VCSToolkit::Objects::Blob.new(content: 'content 9'),
     }
   end
 
   let(:tree) do
     double('VCSToolkit::Objects::Tree').tap do |tree|
       allow(tree).to receive(:all_files) { tree_files }
+    end
+  end
+
+  let(:new_tree) do
+    double('VCSToolkit::Objects::Tree').tap do |tree|
+      allow(tree).to receive(:all_files) { new_tree_files }
     end
   end
 
@@ -90,6 +106,82 @@ describe VCSToolkit::Utils::Status do
           'lib/vcs_toolkit/utils/object_store.rb',
           'lib/vcs_toolkit/objects/object1.rb',
         ]
+      end
+    end
+  end
+
+  describe '.compare_trees' do
+    context 'without ignore' do
+      subject(:status) do
+        VCSToolkit::Utils::Status.compare_trees tree, new_tree, object_store
+      end
+
+      it 'detects created files' do
+        expect(status[:created]).to match_array [
+          'README_new.md',
+          'lib/vcs_toolkit/objects/object1.rb',
+        ]
+      end
+
+      it 'detects deleted files' do
+        expect(status[:deleted]).to match_array [
+          'README.md',
+          'lib/vcs_toolkit/objects/object.rb',
+          'lib/vcs_toolkit/utils/memory_store.rb',
+        ]
+      end
+
+      it 'detects changed files' do
+        expect(status[:changed]).to match_array [
+          'lib/vcs_toolkit.rb',
+        ]
+      end
+    end
+
+    context 'with ignore' do
+      it 'ignores files and directories' do
+        expect(tree) .to receive(:all_files).with(object_store, ignore: [/_store\.rb$/])
+        expect(new_tree).to receive(:all_files).with(object_store, ignore: [/_store\.rb$/])
+
+        status = VCSToolkit::Utils::Status.compare_trees tree,
+                                                         new_tree,
+                                                         object_store,
+                                                         ignore: [/_store\.rb$/]
+      end
+    end
+
+    context 'with nil base tree' do
+      subject(:status) do
+        VCSToolkit::Utils::Status.compare_trees nil, new_tree, object_store
+      end
+
+      it 'detects all files as new' do
+        expect(status[:deleted]).to be_empty
+        expect(status[:changed]).to be_empty
+        expect(status[:created]).to match_array [
+          'README_new.md',
+          'lib/vcs_toolkit.rb',
+          'lib/vcs_toolkit/utils/object_store.rb',
+          'lib/vcs_toolkit/objects/object1.rb',
+        ]
+      end
+    end
+
+    context 'with nil new tree' do
+      subject(:status) do
+        VCSToolkit::Utils::Status.compare_trees tree, nil, object_store
+      end
+
+      it 'detects all files as deleted' do
+        expect(status[:deleted]).to match_array [
+          'README.md',
+          'lib/vcs_toolkit.rb',
+          'lib/vcs_toolkit/utils/memory_store.rb',
+          'lib/vcs_toolkit/utils/object_store.rb',
+          'lib/vcs_toolkit/objects/object.rb',
+        ]
+        expect(status[:changed]).to be_empty
+        expect(status[:created]).to be_empty
       end
     end
   end
